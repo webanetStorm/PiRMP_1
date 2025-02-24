4e2e063b3aee03f1d63b4fb5b3697e1320842b33
webanet <my2712@mail.ru>
2025-02-24 22:39:20 +0300

    Убрал SideActivity, добавил xml лэйаут для каждого компонента

--- app/src/main/java/com/example/yaskevichmd/MainActivity.java
+++ app/src/main/java/com/example/yaskevichmd/MainActivity.java
@@ -1,12 +1,13 @@
 package com.example.yaskevichmd;
 
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 
 public class MainActivity extends AppCompatActivity
@@ -16,17 +17,13 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate( Bundle savedInstanceState )
     {
         super.onCreate( savedInstanceState );
+        EdgeToEdge.enable( this );
         setContentView( R.layout.activity_main );
-
-        Button button1 = (Button) findViewById( R.id.button );
-        button1.setOnClickListener( new View.OnClickListener()
+        ViewCompat.setOnApplyWindowInsetsListener( findViewById( R.id.main ), ( v, insets ) ->
         {
-            @Override
-            public void onClick( View view )
-            {
-                Intent intent = new Intent( view.getContext(), SideActivity.class );
-                view.getContext().startActivity( intent );
-            }
+            Insets systemBars = insets.getInsets( WindowInsetsCompat.Type.systemBars() );
+            v.setPadding( systemBars.left, systemBars.top, systemBars.right, systemBars.bottom );
+            return insets;
         } );
     }
 
